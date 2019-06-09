2a0c14cd49b4bf7f9e3302713c22ec3b7406b303
Bryan Lindsey <blinz117@gmail.com>
2019-06-09 09:30:25 -0500

    Add network call to fetch trivia questions
    This includes the basic setup for the call, including retrofit + moshi dependencies, internet permissions in the manifest, and some model classes for deserializing the data

---
+++ app/src/main/java/dev/bryanlindsey/trivia/remote/response/TriviaApiResponse.kt
@@ -0,0 +1,5 @@
+package dev.bryanlindsey.trivia.remote.response
+
+data class TriviaApiResponse(
+    val response_code: Int,
+    val results: List<TriviaApiResult>)
---
+++ app/src/main/java/dev/bryanlindsey/trivia/remote/service/TriviaService.kt
@@ -0,0 +1,14 @@
+package dev.bryanlindsey.trivia.remote.service
+
+import dev.bryanlindsey.trivia.remote.response.TriviaApiResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val BASE_URL = "https://opentdb.com/"
+
+interface TriviaService {
+
+    @GET("api.php")
+    fun getTriviaQuestions(@Query("amount") numberOfQuestions: Int): Call<TriviaApiResponse>
+}

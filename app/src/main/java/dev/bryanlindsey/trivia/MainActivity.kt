a6a9e8a6f6f027fce7a1f7f3476aaef2a8c98e26
Bryan Lindsey <blinz117@gmail.com>
2019-06-11 16:30:06 -0500

    Show questions from response with new layout
    The code to build these views is not really how ideally I'd like to do this, but it was quick and easy to write it this way just to get things showing on the screen roughly how I want them to be there.

--- app/src/main/java/dev/bryanlindsey/trivia/MainActivity.kt
+++ app/src/main/java/dev/bryanlindsey/trivia/MainActivity.kt
@@ -1,6 +1,9 @@
 package dev.bryanlindsey.trivia
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,7 +11,7 @@ import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val triviaQuestionDisplayViewModel : TriviaQuestionDisplayViewModel by viewModel()
+    private val triviaQuestionDisplayViewModel: TriviaQuestionDisplayViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,29 @@ class MainActivity : AppCompatActivity() {
         triviaQuestionDisplayViewModel.triviaQuestionsLiveData.observe(
             this,
             Observer {
-                triviaQuestionDisplayText.text = it
+                for (question in it.results) {
+                    val triviaView = layoutInflater.inflate(R.layout.trivia_question, questionItemContainer, false)
+
+                    val questionText = triviaView.findViewById<TextView>(R.id.questionText)
+                    questionText.text = question.question
+
+                    val answerGroup = triviaView.findViewById<RadioGroup>(R.id.answersGroup)
+                    answerGroup.addView(
+                        RadioButton(this).apply {
+                            text = question.correct_answer
+                        }
+                    )
+
+                    for (incorrectAnswer in question.incorrect_answers) {
+                        answerGroup.addView(
+                            RadioButton(this).apply {
+                                text = incorrectAnswer
+                            }
+                        )
+                    }
+
+                    questionItemContainer.addView(triviaView)
+                }
             }
         )
 
--- app/src/main/java/dev/bryanlindsey/trivia/TriviaQuestionDisplayViewModel.kt
+++ app/src/main/java/dev/bryanlindsey/trivia/TriviaQuestionDisplayViewModel.kt
@@ -4,22 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dev.bryanlindsey.trivia.remote.response.TriviaApiResponse
 import dev.bryanlindsey.trivia.remote.service.TriviaService
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class TriviaQuestionDisplayViewModel(private val triviaService: TriviaService) : ViewModel() {
 
-    private val _triviaQuestionsLiveData = MutableLiveData<String>()
+    private val _triviaQuestionsLiveData = MutableLiveData<TriviaApiResponse>()
 
-    var triviaQuestionsLiveData: LiveData<String> = _triviaQuestionsLiveData
+    var triviaQuestionsLiveData: LiveData<TriviaApiResponse> = _triviaQuestionsLiveData
 
     fun getMoreTriviaQuestions() =
             viewModelScope.launch {
                 runBlocking {
                     val response = triviaService.getTriviaQuestions(10)
 
-                    _triviaQuestionsLiveData.postValue(response.toString())
+                    _triviaQuestionsLiveData.postValue(response)
                 }
             }
 }

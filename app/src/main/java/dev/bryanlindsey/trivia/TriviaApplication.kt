749932a0fb2181b14e9d64250cc2b46e650e426a
Bryan Lindsey <blinz117@gmail.com>
2019-06-12 16:16:45 -0500

    Enable dark mode by default
    It would be nice to eventually add something to let you switch modes, but I mostly want to try out working in dark mode to see how it is different, so forcing it to on for now should be good enough to experiment with at first.

--- app/src/main/java/dev/bryanlindsey/trivia/TriviaApplication.kt
+++ app/src/main/java/dev/bryanlindsey/trivia/TriviaApplication.kt
@@ -1,6 +1,8 @@
 package dev.bryanlindsey.trivia
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import dev.bryanlindsey.trivia.remote.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -9,6 +11,8 @@ class TriviaApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
 
         startKoin {
             androidContext(this@TriviaApplication)

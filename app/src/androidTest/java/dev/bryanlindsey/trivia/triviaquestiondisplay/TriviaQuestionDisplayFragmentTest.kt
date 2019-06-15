06639589b12eb05563b5eb81ecfa15dfea30e4f2
Bryan Lindsey <blinz117@gmail.com>
2019-06-15 04:39:31 -0500

    Use correct runner import
    The old one is deprecated

--- app/src/androidTest/java/dev/bryanlindsey/trivia/triviaquestiondisplay/TriviaQuestionDisplayFragmentTest.kt
+++ app/src/androidTest/java/dev/bryanlindsey/trivia/triviaquestiondisplay/TriviaQuestionDisplayFragmentTest.kt
@@ -7,7 +7,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import dev.bryanlindsey.trivia.R
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer

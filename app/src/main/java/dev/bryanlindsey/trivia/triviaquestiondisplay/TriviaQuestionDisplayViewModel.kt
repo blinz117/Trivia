6b33c3576d2930b7a59a72a83b8213e503ff5052
Bryan Lindsey <blinz117@gmail.com>
2019-07-07 19:08:17 -0500

    Use value for number of questions from preferences

--- app/src/main/java/dev/bryanlindsey/trivia/triviaquestiondisplay/TriviaQuestionDisplayViewModel.kt
+++ app/src/main/java/dev/bryanlindsey/trivia/triviaquestiondisplay/TriviaQuestionDisplayViewModel.kt
@@ -1,16 +1,20 @@
 package dev.bryanlindsey.trivia.triviaquestiondisplay
 
+import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.bryanlindsey.trivia.remote.response.TriviaApiResponse
 import dev.bryanlindsey.trivia.remote.service.TriviaService
+import dev.bryanlindsey.trivia.settings.NUMBER_OF_QUESTIONS_PREFERENCE_KEY
 import kotlinx.coroutines.launch
 
 private const val DEFAULT_QUESTION_COUNT = 10
 
-class TriviaQuestionDisplayViewModel(private val triviaService: TriviaService) : ViewModel() {
+class TriviaQuestionDisplayViewModel(
+    private val triviaService: TriviaService,
+    private val preferences: SharedPreferences) : ViewModel() {
 
     private val _triviaQuestionsLiveData = MutableLiveData<TriviaApiResponse>()
 
@@ -22,7 +26,9 @@ class TriviaQuestionDisplayViewModel(private val triviaService: TriviaService) :
 
     fun getMoreTriviaQuestions() =
         viewModelScope.launch {
-            val response = triviaService.getTriviaQuestions(DEFAULT_QUESTION_COUNT)
+            val numberOfQuestions = preferences.getInt(NUMBER_OF_QUESTIONS_PREFERENCE_KEY, DEFAULT_QUESTION_COUNT)
+
+            val response = triviaService.getTriviaQuestions(numberOfQuestions)
 
             _triviaQuestionsLiveData.value = response
         }

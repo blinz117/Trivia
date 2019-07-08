3a7030490a4e025d2113e5e17bc13d0e3d331b18
Bryan Lindsey <blinz117@gmail.com>
2019-07-07 19:07:27 -0500

    Add slider for number of questions on settings scr

--- app/src/main/java/dev/bryanlindsey/trivia/settings/SettingsFragment.kt
+++ app/src/main/java/dev/bryanlindsey/trivia/settings/SettingsFragment.kt
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SeekBarPreference
 import androidx.preference.SwitchPreferenceCompat
 import dev.bryanlindsey.trivia.DEFAULT_DARK_MODE_SETTING
+
+const val NUMBER_OF_QUESTIONS_PREFERENCE_KEY = "number_of_questions"
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -30,6 +33,18 @@ class SettingsFragment : PreferenceFragmentCompat() {
             }
 
         screen.addPreference(themePreference)
+
+        val numberOfQuestionsPreference = SeekBarPreference(context).apply {
+            key = NUMBER_OF_QUESTIONS_PREFERENCE_KEY
+            title = "Questions per round"
+            min = 1
+            max = 20
+            showSeekBarValue = true
+            setDefaultValue(10)
+        }
+
+        screen.addPreference(numberOfQuestionsPreference)
+
         preferenceScreen = screen
     }
 }
